0cb42bd78b58174716b3bf00b403bbe14d2b280d
MFBB 555 <benbrahimmohamed24@gmail.com>
2024-11-10 13:54:23 +0100

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component,OnInit, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent, WelcomeComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
